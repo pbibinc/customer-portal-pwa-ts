5871f7eca431757b19a2a3338be0b9976ac6b528
Reinald John Vibar <reinaldvibar@gmail.com>
2025-04-29 15:11:35 -0700

    patch for display

--- src/components/iframe-wrapper/single-components/Preview.tsx
+++ src/components/iframe-wrapper/single-components/Preview.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
 const Preview: React.FC = () => {
   return (
     <>
-      <div className="preview-hero-area">
+      {/* <div className="preview-hero-area">
         <div className="container demo-container direction-rtl">
           <div className="row g-2 align-items-center justify-content-center">
             <div className="text-center">
@@ -11,7 +12,9 @@ const Preview: React.FC = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
+
+      <Navigate to="/login" replace />
     </>
   );
 };
