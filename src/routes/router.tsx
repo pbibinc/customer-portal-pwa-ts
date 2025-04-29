b96f67c77f5a68e8197e05baf26190c4a33c4819
Reinald John Vibar <reinaldvibar@gmail.com>
2025-04-29 15:49:20 -0700

    patch for router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -18,7 +18,7 @@ import ChangePasswordIndex from "../components/change-password";
 
 const routes = createBrowserRouter([
   {
-    path: "/login",
+    path: "/",
     element: <IframeWrapper />,
   },
   {
